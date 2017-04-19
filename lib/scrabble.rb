074af096c65807e3fad768da26cb3c0af9e5f022
jin camou and Steven Galvin <steven@>
2017-04-19 10:13:35 -0700

    build array of letters worth one point. test and confirm that inputted letter is within array, return 1 point.

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,9 +1,10 @@
 class String
   define_method(:scrabble) do
-    total_array = []
-    if self.==("a")
-      total_array.push(1)
+    score_1_letters = ["a", "e", "i", "o", "u", "l", "n", "r", "s", "t"]
+    total_score_array = []
+    if score_1_letters.include?(self)
+      total_score_array.push(1)
     end
-    total_array.join().to_i()
+    total_score_array.join().to_i()
   end
 end
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -3,9 +3,13 @@ require('scrabble')
 
 describe('String#scrabble') do
 
-  it('returns a scrabble score for a letter') do
+  it('returns the corresponding integer for a letter') do
     expect("a".scrabble()).to(eq(1))
   end
 
-  
+  it('returns the corresponding integer for letters "a", "e", "i", "o", "u", "l", "n", "r", "s", "t"') do
+    expect("l".scrabble()).to(eq(1))
+  end
+
+
 end
